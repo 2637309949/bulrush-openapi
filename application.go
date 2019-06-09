e14578008d3c61458ef41a566e18d4fe8df0a095
double <2637309949@qq.com>
2019-06-09 18:23:49 +0800

    feat: update

--- application.go
+++ application.go
@@ -9,20 +9,30 @@
 package openapi
 
 import (
+	"fmt"
 	"net/http"
+	"sort"
 
 	"github.com/2637309949/bulrush"
 	"github.com/gin-gonic/gin"
+	"github.com/thoas/go-funk"
 )
 
 type (
 	// OpenAPI for third party dev
 	OpenAPI struct {
-		URLPrefix string
 		bulrush.PNBase
+		URLPrefix string
+		apis      []Handler
+		Auth      func(appid string) (*OpenAPI, error)
 	}
-	// Params api params
-	Params struct {
+	// AppKeySecret app key secret
+	AppKeySecret struct {
+		AppKey    string
+		AppSecret string
+	}
+	// CRP Common request parameter
+	CRP struct {
 		AppID        string `form:"app_id" json:"app_id" xml:"app_id" binding:"required"`
 		Method       string `form:"method" json:"method" xml:"method" binding:"required"`
 		Format       string `form:"format" json:"format" xml:"format"`
@@ -36,13 +46,31 @@ type (
 		AppAuthToken string `form:"app_auth_token" json:"app_auth_token" xml:"app_auth_token"`
 		BizContent   string `form:"biz_content" json:"biz_content" xml:"biz_content" binding:"required"`
 	}
+	// CRPRet return after voke func
+	CRPRet struct {
+		ReturnURL string
+		NotiMess  *NotiMess
+		Body      interface{}
+	}
+	// NotiMess noti body
+	NotiMess struct {
+		URL  string
+		Mess map[string]interface{}
+	}
+	// Voke func
+	Voke func(*OpenAPI, *CRP) (*CRPRet, error)
+
+	// Handler api handler
+	Handler struct {
+		Name    string
+		Version string
+		Voke    Voke
+	}
 )
 
 // Plugin for Recovery
 // Request Params
-// app_id         String           是     分配给开发者的应用ID 2014072300007148
-// method         String           是     接口名称 xxx.xxx.xx.xx
-// format         String           否     仅支持JSON JSON
+// ihgjb 1111  sefld134r34eruwiru2323rjisfjsd
 // return_url     String           否     HTTP/HTTPS开头字符串 https://xx.xx.com/xx
 // charset        String           是     请求使用的编码格式，如utf 8,gbk,gb2312等 utf 8
 // sign_type      String           是     生成签名字符串所使用的签名算法类型，RSA2和RSA，推荐使用RSA2
@@ -53,22 +81,85 @@ type (
 // app_auth_token String           否     应用授权
 // biz_content    String           是     请求参数的集合，最大长度不限，除公共参数外所有请求参数都必须放在这个参数中传递
 func (openapi *OpenAPI) Plugin() bulrush.PNRet {
-	return func(cfg *bulrush.Config, router *gin.RouterGroup) {
-		router.GET(openapi.URLPrefix, func(c *gin.Context) {
-			var puData Params
-			if error := c.ShouldBindQuery(&puData); error != nil {
-				c.JSON(http.StatusInternalServerError, gin.H{"message": error.Error()})
-				return
-			}
-			c.JSON(http.StatusOK, puData)
-		})
-		router.POST(openapi.URLPrefix, func(c *gin.Context) {
-			var puData Params
-			if error := c.ShouldBind(&puData); error != nil {
-				c.JSON(http.StatusInternalServerError, gin.H{"message": error.Error()})
-				return
-			}
-			c.JSON(http.StatusOK, puData)
-		})
+	return func(cfg *bulrush.Config, router *gin.RouterGroup) *OpenAPI {
+		router.POST(openapi.URLPrefix, openapi.reqHandle)
+		return openapi
 	}
+}
+
+// RegistHandler for register open handler
+func (openapi *OpenAPI) RegistHandler(h Handler) (bool, error) {
+	existedHandler := funk.Find(openapi.apis, func(handler Handler) bool {
+		return (handler.Name == h.Name) && (handler.Version == h.Version)
+	})
+	if existedHandler != nil {
+		return false, fmt.Errorf("Handler has existedd %s", existedHandler)
+	}
+	openapi.apis = append(openapi.apis, h)
+	return true, nil
+}
+
+// handle http request
+func (openapi *OpenAPI) reqHandle(c *gin.Context) {
+	// params check
+	var puData CRP
+	if err := c.ShouldBindQuery(&puData); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+		c.Abort()
+		return
+	}
+	// auth check
+	appKeySecret, err := openapi.authenticate(&puData, c)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+		c.Abort()
+		c.Abort()
+		return
+	}
+	// find openapi
+	voke, err := openapi.findVoke(puData.Method, puData.Version)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+		c.Abort()
+		return
+	}
+	// voke openapi
+	ret, err := voke(appKeySecret, &puData)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+		c.Abort()
+		return
+	}
+
+	if ret.ReturnURL != "" {
+		c.Redirect(http.StatusMovedPermanently, "/auth/login")
+		c.Abort()
+		return
+	}
+	if ret.NotiMess != nil {
+	}
+	c.JSON(http.StatusOK, ret.Body)
+}
+
+func (openapi *OpenAPI) findVoke(method string, version string) (Voke, error) {
+	return nil, nil
+}
+
+func (openapi *OpenAPI) authenticate(puData *CRP, c *gin.Context) (*OpenAPI, error) {
+	var puJSON map[string]interface{}
+	var puKeys = make([]string, 0, len(puJSON))
+	c.BindJSON(&puJSON)
+
+	// check appid
+	appKeySecret, err := openapi.Auth(puData.AppID)
+	if err != nil {
+		return nil, err
+	}
+
+	// check rsa
+	for k := range puJSON {
+		puKeys = append(puKeys, k)
+	}
+	sort.Strings(puKeys)
+	return appKeySecret, nil
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.12
 require (
 	github.com/2637309949/bulrush v0.0.0-20190609040432-559afa401090
 	github.com/gin-gonic/gin v1.4.0
+	github.com/thoas/go-funk v0.4.0
 )
 
 replace golang.org/x/text v0.3.0 => github.com/golang/text v0.3.0

b211f5a5b312b1da516bd3372a18e969e372922d
double <2637309949@qq.com>
2019-06-14 10:06:26 +0800

    style: 代码优化

--- application.go
+++ application.go
@@ -1,10 +1,6 @@
-/**
- * @author [Double]
- * @email [2637309949@qq.com.com]
- * @create date 2019-01-12 22:46:31
- * @modify date 2019-01-12 22:46:31
- * @desc [bulrush openapi]
- */
+// Copyright (c) 2018-2020 Double.  All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
 
 package openapi
 
@@ -70,56 +66,42 @@ type (
 )
 
 // Plugin for OpenAPI
-// Request Params
-// app_id         String           是     分配给开发者的应用ID
-// method         String           是     接口名称 xxx.xxx.xx.xx
-// format         String           否     仅支持JSON JSON
-// return_url     String           否     HTTP/HTTPS开头字符串 https://xx.xx.com/xx
-// charset        String           是     请求使用的编码格式，如utf 8,gbk,gb2312等 utf 8
-// sign_type      String           是     生成签名字符串所使用的签名算法类型，RSA2和RSA，目前RSA
-// sign           String           是     请求参数的签名串
-// timestamp      String           是     发送请求的时间，格式"yyyy MM dd HH:mm:ss" 2014 07 24 03:07:50
-// version        String           是     调用的接口版本，固定为：1.0 1.0
-// notify_url     String           否     服务器主动通知商户服务器里指定的页面http/https路径。
-// app_auth_token String           否     应用授权
-// biz_content    String           是     请求参数的集合，最大长度不限，除公共参数外所有请求参数都必须放在这个参数中传递
-func (openapi *OpenAPI) Plugin() bulrush.PNRet {
+func (api *OpenAPI) Plugin() bulrush.PNRet {
 	return func(cfg *bulrush.Config, router *gin.RouterGroup) *OpenAPI {
 		funk.ForEach([]func(string, ...gin.HandlerFunc) gin.IRoutes{router.GET, router.POST}, func(httpMethod func(string, ...gin.HandlerFunc) gin.IRoutes) {
-			httpMethod(openapi.URLPrefix, openapi.requestHandle)
+			httpMethod(api.URLPrefix, api.requestHandle)
 		})
-		return openapi
+		return api
 	}
 }
 
 // RegistHandler defines for register open handler
-func (openapi *OpenAPI) RegistHandler(h Handler) (bool, error) {
-	existedHandler := funk.Find(openapi.apis, func(handler Handler) bool {
+func (api *OpenAPI) RegistHandler(h Handler) (bool, error) {
+	if existedHandler := funk.Find(api.apis, func(handler Handler) bool {
 		return (handler.Name == h.Name) && (handler.Version == h.Version)
-	})
-	if existedHandler != nil {
+	}); existedHandler != nil {
 		rushLogger.Warn("handler has existedd %s", existedHandler)
 		return false, fmt.Errorf("handler has existedd %s", existedHandler)
 	}
-	openapi.apis = append(openapi.apis, h)
+	api.apis = append(api.apis, h)
 	return true, nil
 }
 
 // handle http request
-func (openapi *OpenAPI) requestHandle(c *gin.Context) {
+func (api *OpenAPI) requestHandle(c *gin.Context) {
 	puData, err := getForm(c)
 	if err != nil {
 		rushLogger.Warn("getForm error %s", err.Error())
 		c.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
 		return
 	}
-	appKeySecret, err := openapi.appAuth(puData, c)
+	appKeySecret, err := api.appAuth(puData, c)
 	if err != nil {
 		rushLogger.Warn("appAuth error %s", err.Error())
 		c.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
 		return
 	}
-	voke, err := openapi.findVoke(puData.Method, puData.Version)
+	voke, err := api.findVoke(puData.Method, puData.Version)
 	if err != nil {
 		rushLogger.Warn("findVoke error %s", err.Error())
 		c.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
@@ -131,22 +113,22 @@ func (openapi *OpenAPI) requestHandle(c *gin.Context) {
 		c.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
 		return
 	}
+	if ret.Noti != nil && ret.Noti.URL != "" {
+		err := api.noti(c, ret.Noti)
+		if err != nil {
+			rushLogger.Error("postRequest error %s", err.Error())
+		}
+	}
 	if ret.RetURL != "" {
 		rushLogger.Info("voke retUrl %s", ret.RetURL)
 		c.Redirect(http.StatusMovedPermanently, ret.RetURL)
 		return
 	}
-	if ret.Noti != nil && ret.Noti.URL != "" {
-		err := openapi.noti(c, ret.Noti)
-		if err != nil {
-			rushLogger.Error("postRequest error %s", err.Error())
-		}
-	}
 	c.JSON(http.StatusOK, ret.Body)
 }
 
 // Post message twice
-func (openapi *OpenAPI) noti(c *gin.Context, noti *Noti) error {
+func (api *OpenAPI) noti(c *gin.Context, noti *Noti) error {
 	_, err := postRequest(noti.URL, noti.News)
 	if err != nil {
 		_, err := postRequest(noti.URL, noti.News)
@@ -155,8 +137,8 @@ func (openapi *OpenAPI) noti(c *gin.Context, noti *Noti) error {
 	return nil
 }
 
-func (openapi *OpenAPI) findVoke(method string, version string) (Voke, error) {
-	h := funk.Find(openapi.apis, func(handler Handler) bool {
+func (api *OpenAPI) findVoke(method string, version string) (Voke, error) {
+	h := funk.Find(api.apis, func(handler Handler) bool {
 		return (handler.Name == method) && (handler.Version == version)
 	})
 	if h == nil {
@@ -167,8 +149,8 @@ func (openapi *OpenAPI) findVoke(method string, version string) (Voke, error) {
 	return v, nil
 }
 
-func (openapi *OpenAPI) appAuth(puData *CRP, c *gin.Context) (*AppInfo, error) {
-	if appKeySecret, err := openapi.Auth(puData.AppID); err == nil {
+func (api *OpenAPI) appAuth(puData *CRP, c *gin.Context) (*AppInfo, error) {
+	if appKeySecret, err := api.Auth(puData.AppID); err == nil {
 		if err := rsaVerify(puData, appKeySecret); err != nil {
 			rushLogger.Warn("rsaVerify error %s", err.Error())
 			return nil, err

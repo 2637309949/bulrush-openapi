7fc8e590af0fafc98942755d4dad782617f376cf
double <2637309949@qq.com>
2019-07-02 20:48:09 +0800

    style: doc

--- application.go
+++ application.go
@@ -66,7 +66,7 @@ type (
 )
 
 // Plugin for OpenAPI
-func (api *OpenAPI) Plugin() bulrush.PNRet {
+func (api *OpenAPI) Plugin() interface{} {
 	return func(cfg *bulrush.Config, router *gin.RouterGroup) *OpenAPI {
 		funk.ForEach([]func(string, ...gin.HandlerFunc) gin.IRoutes{router.GET, router.POST}, func(httpMethod func(string, ...gin.HandlerFunc) gin.IRoutes) {
 			httpMethod(api.URLPrefix, api.requestHandle)
